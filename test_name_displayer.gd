6c4fc289159dd962c502f7e740c8ba483902cb57
Chris Tutorials <christutorialsyt@gmail.com>
2024-12-28 14:50:10 -0500

    rename node_naming to name_displayer

--- test_name_displayer.gd
+++ test_name_displayer.gd
@@ -1,13 +1,13 @@
 extends GdUnitTestSuite
 
-var node_naming : NodeNaming
+var name_displayer : NameDisplayer
 var test_node : Node
 
 
 var building_node_script = preload("res://addons/grid_building/components/building_node.gd")
 
 func before_test():
-	node_naming = NodeNaming.new()
+	name_displayer = NameDisplayer.new()
 	test_node = auto_free(Node.new())
 	add_child(test_node)
 	
@@ -16,8 +16,8 @@ func test_get_display_name(p_name : String, p_method_name : String, p_ex : Strin
 	["TestNode_500", "to_string", "TestNode_500:<Node", true]
 ]):
 	test_node.name = p_name
-	node_naming.custom_name_method = p_method_name
-	var display_name = node_naming.get_display_name(test_node)
+	name_displayer.custom_name_method = p_method_name
+	var display_name = name_displayer.get_display_name(test_node)
 	
 	if p_ex_start_with:
 		assert_str(display_name).starts_with(p_ex)
@@ -30,5 +30,5 @@ func test_building_node_get_display_name(p_name : String, p_ex : String, test_pa
 ]):
 	var building_node : Node = auto_free(building_node_script.new())
 	building_node.name = p_name
-	var display_name = node_naming.get_display_name(building_node)
+	var display_name = name_displayer.get_display_name(building_node)
 	assert_str(display_name).is_equal(p_ex)
