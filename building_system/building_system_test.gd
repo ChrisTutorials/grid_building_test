51e24e4c461117d36f7c0d46c03cd60da5cb7f91
Chris' Tutorials <christutorialsyt@gmail.com>
2025-07-24 19:51:45 -0400

    refractoring towards config files rather than too many exports for resources

---
+++ building_system/building_system_test.gd
@@ -0,0 +1,217 @@
+# GdUnit generated TestSuite
+class_name BuildingSystemTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+
+var system : BuildingSystem
+var targeting_state : GridTargetingState
+var user_state : UserState
+var rci_manager : RuleCheckIndicatorManager
+var mode_state : ModeState
+var grid_positioner : Node2D
+var tile_map : TileMap
+var tile_set : TileSet
+var placer : Node2D
+var placed_parent : Node2D
+var building_actions : BuildingActions
+
+var placeable_instance_script : Script = load("uid://dvt7wrugafo5o")
+var placeable_2d_test : Placeable = load("uid://jgmywi04ib7c")
+var default_preview_script : GDScript = load("uid://cufp4o5ctq6ak")
+
+func before_test():
+	placer = auto_free(Node2D.new())
+	add_child(placer)
+	
+	placed_parent = auto_free(Node2D.new())
+	add_child(placed_parent)
+	
+	
+	grid_positioner = auto_free(Node2D.new())
+	add_child(grid_positioner)
+	
+	tile_map = auto_free(TileMap.new())
+	add_child(tile_map)
+	tile_set = TileSet.new()
+	tile_map.tile_set = tile_set
+	
+	targeting_state = auto_free(GridTargetingState.new())
+	targeting_state.positioner = grid_positioner
+	targeting_state.target_map = tile_map
+	targeting_state.maps = [tile_map]
+	
+	mode_state = ModeState.new()
+	system = auto_free(BuildingSystem.new())
+	building_actions = BuildingActions.new()
+	system.actions = building_actions
+	system.mode_actions = ModeInputActions.new()
+	system.mode_state = mode_state
+	system.state = BuildingState.new()
+	system.placement_validator = PlacementValidator.new()
+	system.targeting_state = targeting_state
+
+	## Turn debug on for testing
+	system.debug = GBDebugSettings.new(true)
+	
+	add_child(system)
+	
+	user_state = UserState.new()
+	user_state.user = placer
+	system.targeting_state.origin_state = user_state
+	
+	
+	system.placement_validator = PlacementValidator.new()
+	rci_manager = auto_free(RuleCheckIndicatorManager.new(TestSceneLibrary.indicator, targeting_state, system.placement_validator))
+	rci_manager.name = "RuleCheckIndicatorManager"
+	grid_positioner.add_child(rci_manager)
+	system.placement_validator.indicator_manager = rci_manager
+	
+	system.state = BuildingState.new()
+	system.state.placer_state = user_state
+	system.state.placed_parent = placed_parent
+	system.settings = TestSceneLibrary.building_settings.duplicate(true)
+	
+func test_before_test_setup():
+	assert_object(system).is_not_null()
+	var problems = system.validate()
+	assert_array(problems).is_empty()
+
+@warning_ignore("unused_parameter")
+func test_instantiate_placeable_preview_fails(p_placeable : Variant, p_warning : String, test_parameters := [
+	[null, system._WARNING_INVALID_PLACEABLE],
+]):
+	var instantiate = func(): system.instantiate_placeable_preview(p_placeable)
+	assert_error(instantiate).is_push_warning(p_warning % p_placeable)
+
+@warning_ignore("unused_parameter")
+func test_instantiate_placeable_preview() -> void:
+	var placeable = TestSceneLibrary.placeable_2d_test
+	var preview = auto_free(system.instantiate_placeable_preview(placeable))
+	assert_object(preview).is_not_null()
+	
+
+func test_remove_scripts() -> void:
+	var preview : Node = auto_free(TestSceneLibrary.placeable_2d_test.packed_scene.instantiate())
+	add_child(preview)
+	var manipulatable = GBSearchUtils.find_first(preview, Manipulatable)
+	assert_object(preview).override_failure_message("Preview not instanced.").is_not_null()
+	assert_object(manipulatable).is_not_null()
+	
+	system.remove_scripts(preview, ["Manipulatable"])
+	
+	var preview_script = preview.get_script()
+	assert_object(preview_script).override_failure_message("[%s] Expected null script but has one" % preview_script).is_null()
+	
+	assert_object(manipulatable).is_not_null()
+	assert_object(manipulatable.get_script()).is_not_null()
+	
+	system.remove_scripts(preview, [])
+	assert_object(manipulatable).is_not_null()
+	assert_object(manipulatable.get_script()).is_null()
+
+func test_unhandled_input():
+	var action_event = InputEventAction.new()
+	action_event.action = building_actions.confirm
+	action_event.pressed = true
+	
+	system._unhandled_input(action_event)
+	
+	mode_state.mode = GBEnums.Mode.BUILD
+
+	action_event.action = system.mode_actions.off_mode
+	system._unhandled_input(action_event)
+
+func test_validate_input_map():
+	var is_input_map_validated = system.validate_input_map()
+	assert_bool(is_input_map_validated).is_true()
+
+func test_set_buildable_preview():
+	var test_placeable = Placeable.new()
+	test_placeable.packed_scene = TestSceneLibrary.box_scripted
+	assert_bool(test_placeable.validate()).is_true()
+	var successful = system.set_buildable_preview(test_placeable)
+	
+	if not successful:
+		fail("Buildable preview should have successfully instanced")
+		return
+		
+	var preview_instance = system.state.preview
+	
+	if not preview_instance:
+		fail("Preview instance should have been instanced. Stopping test.")
+		return 
+	
+	var source_code : String = preview_instance.get_script().source_code
+	assert_str(source_code).is_equal(default_preview_script.source_code)
+	
+	for child in preview_instance.get_children():
+		assert_object(child.get_script()).is_null()
+
+@warning_ignore("unused_parameter")
+func test_set_buildable_preview_keep_script_test(p_script : Script, test_parameters := [
+	[placeable_instance_script]
+]):
+	var placeable_test = Placeable.new()
+	var scripted_node : Node = auto_free(TestSceneLibrary.keep_script_scene.instantiate())
+	var packed_scene = PackedScene.new()
+	
+	# Execution
+	var child_node = scripted_node.get_child(0)
+	assert_object(child_node.owner).is_same(scripted_node)
+	packed_scene.pack(scripted_node)
+	placeable_test.packed_scene = TestSceneLibrary.keep_script_scene
+	var script_type = p_script.get_global_name()
+	system.settings.preview_kept_script_types.append(script_type)
+	
+	scripted_node.free()
+	var is_preview_set = system.set_buildable_preview(placeable_test)
+	assert_bool(is_preview_set).is_true()
+	var preview_instance = system.state.preview
+	
+	assert_object(preview_instance).append_failure_message("Preview instanced should be a valid node.").is_instanceof(Node2D)
+	assert_object(preview_instance.get_script()).is_same(p_script)
+	
+	for child in preview_instance.get_children():
+		assert_object(child.get_script()).is_same(p_script)
+
+@warning_ignore("unused_parameter")
+func test_try_build(p_placeable : Placeable, ex_null_result : bool, test_parameters := [
+	[null, true],
+	[placeable_2d_test, false]
+]) -> void:
+	system.selected_placeable = p_placeable
+	
+	if p_placeable != null && p_placeable.packed_scene != null:
+		system.state.preview = p_placeable.packed_scene.instantiate()
+		add_child(system.state.preview)
+	
+	var result = system.try_build()
+	assert_bool(result == null).is_equal(ex_null_result)
+
+@warning_ignore("unused_parameter")
+func test__build(p_placeable : Placeable, p_expected : Variant, test_parameters := [
+	[null, null],
+	[load("uid://jgmywi04ib7c"), any_object()]
+]) -> void:
+	system.selected_placeable = p_placeable
+	
+	if p_placeable != null && p_placeable.packed_scene != null:
+		system.state.preview = auto_free(p_placeable.packed_scene.instantiate())
+		add_child(system.state.preview)
+	
+	var result = system._build()
+	assert_object(result).is_equal(p_expected)
+
+## Creates a node and a child node with a script attached
+## Returns the root
+func create_node_and_child_with_script(p_script : Script) -> Node:
+	var root = auto_free(Node.new())
+	add_child(root)
+	root.set_script(p_script)
+	var child = auto_free(Node.new())
+	root.add_child(child)
+	child.set_script(p_script)
+	return root
---
+++ grid_targeting_system/grid_targeting_system_test/grid_targeting_system_test.gd
@@ -0,0 +1,142 @@
+# GdUnit generated TestSuite
+class_name GridTargetingSystemTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+
+var library : TestSceneLibrary
+var system : GridTargetingSystem
+var state : GridTargetingState
+var settings : GridTargetingSettings
+var positioner : Node2D
+var placer : Node2D
+var placed_parent : Node2D
+var tile_map : TileMap
+var vec_max_tile_distance : Vector2
+
+func before_test():
+	state = GridTargetingState.new()
+	
+	positioner = auto_free(Node2D.new())
+	add_child(positioner)
+	
+	placer = auto_free(Node2D.new())
+	add_child(placer)
+	
+	placed_parent = auto_free(Node2D.new())
+	add_child(placed_parent)
+	
+	tile_map = auto_free(library.tile_map_buildable.instantiate())
+	add_child(tile_map)
+	
+	state.target_map = tile_map
+	state.maps = [tile_map]
+	state.positioner = positioner
+	var origin_state = UserState.new()
+	state.origin_state = origin_state
+	origin_state.user = placer
+	assert_array(state.validate()).append_failure_message("Problems in setup found").is_empty()
+	
+	settings = library.grid_targeting_settings.duplicate(true)
+	
+	assert_array(settings.validate()).append_failure_message("Settings passes validation check.").is_empty()
+	system = auto_free(GridTargetingSystem.new())
+	system.state = state
+	system.settings = settings
+	system.mode_state = ModeState.new()
+
+	vec_max_tile_distance = Vector2(settings.max_tile_distance, settings.max_tile_distance)
+
+func test_has_valid_setup():
+	assert_object(system.state).append_failure_message("Building state must be set.").is_not_null()
+	assert_array(system.state.validate()).is_empty()
+	assert_object(system.astar_grid).append_failure_message("Astar grid was not generated").is_not_null()
+	assert_vector(system.astar_grid.region.size).is_equal(Vector2i(50,50))
+	assert_int(settings.max_tile_distance).append_failure_message("Tests by default expect this to be 3").is_equal(3)
+
+# Test the System's astar grid to see that certain locations are in or out of bounds
+@warning_ignore("unused_parameter")
+func test_is_in_bounds(p_tile_location : Vector2i, p_expected : bool, test_parameters := [
+	[Vector2i(49,49), true],
+	[Vector2i(50,50), false],
+	[Vector2i(30,30), true],
+	[Vector2i(21,21), true],
+	[Vector2i(20,20), true],
+	[Vector2i(0,0), true],
+	[Vector2i(-20,-20), false],
+	[Vector2i(-20, 20), false],
+	[Vector2i(20, -20), false]
+
+]) -> void:
+	var is_in_bounds = system.astar_grid.is_in_bounds(p_tile_location.x, p_tile_location.y)
+	assert_bool(is_in_bounds).is_equal(p_expected)
+
+@warning_ignore("unused_parameter")
+func test_get_max_tile_distance_tile_to_target(p_tile_location : Vector2i, p_expected_in_bounds : bool, p_expected_tile_distance : Variant, test_parameters := [
+	[Vector2i(50,50), false, null],
+	[Vector2i(20,20), true, vec_max_tile_distance]
+]) -> void:
+	system.mode_state.mode = GBEnums.Mode.BUILD
+	assert_that(system.mode_state.mode).append_failure_message("Should be in build mode.").is_equal(GBEnums.Mode.BUILD)
+	
+	var is_in_bounds = system.astar_grid.is_in_bounds(p_tile_location.x, p_tile_location.y)
+	assert_bool(is_in_bounds).is_equal(p_expected_in_bounds)
+	
+	system.update_astar_grid_2d(system.astar_grid, system.settings)
+	positioner.global_position = Vector2.ZERO ## RESET POSITION
+	
+	var tile_distance = system.get_max_tile_distance_tile_to_target(positioner, p_tile_location)
+	assert_that(tile_distance).is_equal(p_expected_tile_distance)
+
+## Test get max tile distance when AStar grid is set to no diaganols and manhattan search pattern
+@warning_ignore("unused_parameter")
+func test_get_max_tile_distance_tile_to_target_no_diaganols(p_test_location : Vector2i, p_expected_tile : Variant, test_parameters := [
+	[Vector2i(-1,0), Vector2.ZERO],
+	[Vector2i(0,-1), Vector2.ZERO],
+	[Vector2i(4, 4), Vector2(3,0)],
+	[Vector2i(1, 4), Vector2(1,2)],
+	[Vector2i(0, 4), Vector2(0,3)]
+]):
+	set_NO_diaganol_manhattan_astar_search_pattern()
+	positioner.global_position = Vector2.ZERO
+
+	var no_diag_limited_tile = system.get_max_tile_distance_tile_to_target(positioner, p_test_location)
+	assert_that(no_diag_limited_tile).is_equal(p_expected_tile)
+
+@warning_ignore("unused_parameter")
+func test_move_node_to_closest_valid_tile(p_target : Vector2i, p_expected_error : int, test_parameters := [
+	[Vector2i(-200,-200), OK]
+]):
+	settings.limit_to_adjacent = false
+	assert_object(state.target_map).append_failure_message("Tile map set to valid value").is_not_null()
+	var off_map_result = system.move_node_to_closest_valid_tile(p_target, positioner, placer)
+	assert_that(off_map_result).append_failure_message("Did not run successfully").is_equal(p_expected_error)
+
+@warning_ignore("unused_parameter")
+func test_snap_tile_to_region(p_snap_tile : Vector2i, p_expected_tile : Vector2i, test_parameters := [
+	[Vector2i(26,26), Vector2i(25,25)],
+	[Vector2i(-26,-26), Vector2i(-25, -25)]
+]):
+	var grid : AStarGrid2D = create_astar_grid_centered_on_0_size_50()
+	var result = system.snap_tile_to_region(p_snap_tile, grid.region)
+	assert_vector(result).is_equal(p_expected_tile)
+
+func set_NO_diaganol_manhattan_astar_search_pattern():
+	settings.diaganol_mode = AStarGrid2D.DiagonalMode.DIAGONAL_MODE_NEVER
+	settings.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	settings.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
+	assert_that(system.astar_grid.diagonal_mode).append_failure_message("AStar Diaganol mode set properly.").is_equal(settings.diaganol_mode)
+	assert_that(system.astar_grid.default_estimate_heuristic).append_failure_message("4 direction calculation expected.").is_equal(AStarGrid2D.HEURISTIC_MANHATTAN)
+	assert_that(system.astar_grid.default_compute_heuristic).append_failure_message("4 direction calculation expected.").is_equal(AStarGrid2D.HEURISTIC_MANHATTAN)
+
+func create_astar_grid_centered_on_0_size_50():
+	var grid = auto_free(AStarGrid2D.new())
+	grid.region = Rect2(-25,-25,50,50)
+	
+	var position = grid.region.position
+	var end = grid.region.end
+	assert_vector(position).append_failure_message("Start should have been half size negative").is_equal(Vector2i(-25,-25))
+	assert_vector(end).append_failure_message("End is half size").is_equal(Vector2i(25,25))
+	return grid
