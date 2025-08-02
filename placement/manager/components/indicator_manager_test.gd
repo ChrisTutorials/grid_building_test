7f2f875b867ef38f60696d6d913ec78995d66fee
Chris' Tutorials <christutorialsyt@gmail.com>
2025-08-02 18:17:41 -0400

    modifying test for indicator manager node -> refCounted refractor

--- placement/manager/components/indicator_manager_test.gd
+++ placement/manager/components/indicator_manager_test.gd
@@ -4,22 +4,26 @@ var indicator_manager: IndicatorManager
 var indicator_template : PackedScene = preload("uid://dhox8mb8kuaxa")
 var received_signal: bool
 var indicator: RuleCheckIndicator
-var _injector : GBInjectorSystem
-var _container : GBCompositionContainer
+var targeting_state: GridTargetingState
+var logger: GBLogger
+var indicator_parent : Node2D
 
 func before_test():
-	_container = load("uid://dy6e5p5d6ax6n")
-	_injector = GBDoubleFactory.create_test_injector(self, _container)
-	indicator_manager = IndicatorManager.new()
-	add_child(indicator_manager) # Must add to scene tree for signals, add_child, free etc to work
+	indicator_parent = Node2D.new()
+	add_child(indicator_parent)
 
-	_initialize_targeting_state(_container.get_targeting_state())
+	# Create dependencies manually
+	targeting_state = GridTargetingState.new(GBOwnerContext.new())
+	logger = GBDoubleFactory.create_test_logger()
+	indicator_manager = IndicatorManager.new(indicator_parent, targeting_state, indicator_template, logger)
+
+	_initialize_targeting_state(targeting_state)
 
 	received_signal = false
 	indicator = null
 
 func after_test():
-	indicator_manager.free()
+	indicator_manager = null
 
 func _create_real_indicator() -> RuleCheckIndicator:
 	var instance = indicator_template.instantiate() as RuleCheckIndicator
@@ -27,28 +31,30 @@ func _create_real_indicator() -> RuleCheckIndicator:
 	add_child(instance) # add to scene so it's in scene tree
 	return instance
 
-func _initialize_targeting_state(targeting_state: GridTargetingState) -> void:
+func _initialize_targeting_state(p_targeting_state: GridTargetingState) -> void:
 	var map := GBDoubleFactory.create_test_tile_map_layer(self)
-	targeting_state.set_map_objects(
+	p_targeting_state.set_map_objects(
 		map, [map]
 	)
 
 	var positioner : Node2D = auto_free(Node2D.new())
-	targeting_state.positioner = positioner
+	p_targeting_state.positioner = positioner
 
 func test_setup_indicators_generates_expected_indicators() -> void:
 	var test_object := Node2D.new()
 	add_child(test_object)
 
-	# Set up a dummy collision shape (required by get_all_collision_objects)
+	# Create a collision object (StaticBody2D) and add a collision shape
+	var body := StaticBody2D.new()
+	test_object.add_child(body)
 	var collision_shape := CollisionShape2D.new()
 	collision_shape.shape = CircleShape2D.new()
-	test_object.add_child(collision_shape)
+	body.add_child(collision_shape)
 
 	var rule := TileCheckRule.new()
 	var rules : Array[TileCheckRule] = [rule]
 
-	var indicators := indicator_manager.setup_indicators(test_object, rules)
+	var indicators := indicator_manager.setup_indicators(test_object, rules, self)
 
 	assert_int(indicators.size()).is_greater(0)
 	assert_int(indicator_manager.get_indicators().size()).is_greater(indicators.size())
@@ -65,12 +71,11 @@ func test_add_indicators_adds_and_emits_signal() -> void:
 	assert_that(indicator in indicator_manager.get_indicators()).is_true()
 	assert_that(received_signal).is_true()
 
-func test_free_indicators_removes_and_frees() -> void:
+func test_reset_frees_indicators() -> void:
 	var ind = _create_real_indicator()
 	indicator_manager.add_indicators([ind])
 	assert_that(ind in indicator_manager.get_indicators()).is_true()
-	
-	indicator_manager._free_indicators([ind]) # note this is private in your code; if it's private,_
+	indicator_manager.reset()
 
 func _on_indicators_changed(updated: Array[RuleCheckIndicator]) -> void:
 	received_signal = true
